7cbff2354e4c1f48e4224dbb5a17fd8570dd1dc6
melsayed8450 <muhammad.abdel-al02@eng-st.cu.edu.eg>
2023-04-15 20:17:57 +0200

    some edits

--- lib/manager/home_controller.dart
+++ lib/manager/home_controller.dart
@@ -5,7 +5,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:profanity_filter/profanity_filter.dart';
 import 'package:http/http.dart' as http;
 
@@ -19,7 +18,7 @@ class HomeController extends GetxController {
   TextEditingController textController = TextEditingController();
   Future<void> getPicture(BuildContext context) async {
     isLoading.value = true;
-    if (await InternetConnectionChecker().hasConnection) {
+   
       try {
         final pickedFile =
             await imagePicker.pickImage(source: ImageSource.gallery);
@@ -46,13 +45,69 @@ class HomeController extends GetxController {
       } else {
         isLoading.value = false;
       }
-    } else {
+    
+  }
+
+  void checkWord() {
+    hasProfanity.value = filter.hasProfanity(textController.text);
+  }
+
+  Future<void> checkImage(BuildContext context) async {
+    try {
+      var url = Uri.parse('https://api.sightengine.com/1.0/check.json');
+      var request = http.MultipartRequest('POST', url);
+
+      request.fields['models'] = 'nudity-2.0';
+      request.fields['api_user'] = '1109846077';
+      request.fields['api_secret'] = '4ExKYMNC5RL569Hs2JSk';
+
+      var stream = http.ByteStream(imageFile!.openRead());
+      var length = await imageFile!.length();
+      var multipartFile =
+          http.MultipartFile('media', stream, length, filename: 'image.jpg');
+      request.files.add(multipartFile);
+
+      var response = await request.send();
+      var responseBody = await response.stream.bytesToString();
+
+      var output = json.decode(responseBody);
+
+      isLoading.value = false;
+      // ignore: avoid_print
+      print(output);
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Results'),
+          content: SizedBox(
+            height: 50,
+            child: Column(
+              children: [
+                Text(
+                  output['nudity']['none'] < 0.05
+                      ? 'The image contains nudity'
+                      : 'The image doesn\'t contain nudity',
+                ),
+                Text(
+                    'Nudity percentage: ${(1 - output['nudity']['none']) * 100}%'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    } catch (e) {
       isLoading.value = false;
       showDialog<String>(
         context: context,
         builder: (BuildContext context) => AlertDialog(
           title: const Text('Error'),
-          content: Text('Check internet connection then try again'),
+          content: Text('Error occured in the API'),
           actions: <Widget>[
             TextButton(
               onPressed: () => Navigator.pop(context),
@@ -62,60 +117,5 @@ class HomeController extends GetxController {
         ),
       );
     }
-  }
-
-  void checkWord() {
-    hasProfanity.value = filter.hasProfanity(textController.text);
-  }
-
-  Future<void> checkImage(BuildContext context) async {
-    var url = Uri.parse('https://api.sightengine.com/1.0/check.json');
-    var request = http.MultipartRequest('POST', url);
-
-    request.fields['models'] = 'nudity-2.0';
-    request.fields['api_user'] = '1109846077';
-    request.fields['api_secret'] = '4ExKYMNC5RL569Hs2JSk';
-
-    var stream = http.ByteStream(imageFile!.openRead());
-    var length = await imageFile!.length();
-    var multipartFile =
-        http.MultipartFile('media', stream, length, filename: 'image.jpg');
-    request.files.add(multipartFile);
-
-    var response = await request.send();
-    var responseBody = await response.stream.bytesToString();
-
-    var output = json.decode(responseBody);
-
-    isLoading.value = false;
-    // ignore: avoid_print
-    print(output);
-    showDialog<String>(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('Results'),
-        content: SizedBox(
-         height: 50,
-          child: Column(
-            children: [
-              Text(
-                output['nudity']['none'] < 0.05
-                    ? 'The image contains nudity'
-                    : 'The image doesn\'t contain nudity',
-              ),
-              Text(
-                'Nudity percentage: ${(1 - output['nudity']['none']) * 100}%'
-              ),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
   }
 }
